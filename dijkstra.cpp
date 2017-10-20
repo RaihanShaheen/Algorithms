150cb3cc1cbcfbe1ea47872ed7480e872b303efe
HP-PC\HP <raihan-s6h@hotmail.com>
2017-10-21 01:01:00 +0300

    Edit Dijkstra

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -10,9 +10,10 @@
 #include <math.h>
 #include <stack>
 using namespace std;
-int const N = 1e4;
-int const INF = 1e7;
+int const N = 3010;
+int const INF = 3e8 + 10;
 int cost[N];
+bool vis[N];
 int n, m, s;
 
 struct st{
@@ -34,25 +35,32 @@ vector<vector<st> >g;
 void dijkstra(){
 
 	priority_queue<st>pq;
-	for (int i = 0; i < n; ++i){
+	for (int i = 0; i <= n; ++i){
 		if (i != s)
 			cost[i] = INF;
-		else
-			cost[i] = 0;
-
-		pq.push(st(i, cost[i]));
+		vis[i] = false;
 	}
+
+	cost[s] = 0;
+	pq.push(st(s, 0));
 
 	while (!pq.empty()){
 		st top = pq.top();
 		pq.pop();
 
 		int from = top.u;
+
+		if (vis[from])
+			continue;
+		vis[from] = true;
+
 		for (int i = 0; i < g[from].size(); ++i){
 			st to = g[top.u][i];
 
 			if (cost[to.u] > to.cost + cost[from]){
 				cost[to.u] = to.cost + cost[from];
+				to.cost = cost[to.u];
+				pq.push(to);
 			}
 
 
@@ -67,10 +75,11 @@ void dijkstra(){
 
 int main(){
 
+	int t;
+
 	scanf("%d%d", &n, &m);
-
+	g.clear();
 	g.resize(n);
-
 	for (int i = 0, x, y, d; i < m; ++i){
 		scanf("%d%d%d", &x, &y, &d);
 		--x; --y;
@@ -92,6 +101,8 @@ int main(){
 			printf("%d ", cost[i]);
 
 	}
+
+	cout << endl;
 
 	return 0;
 }
